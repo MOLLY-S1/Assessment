9f32b2275417f80e3ccb00d2402af41bee9d4585
MOLLY-S1 <sankeym@middleton.school.nz>
2022-05-10 09:13:01 +1200

    final question and base update

--- 00_MQ_Base_V1.py
+++ 00_MQ_Base_V1.py
@@ -2,6 +2,7 @@
 Functions added when they have been tested and completed
 """
 
+import random
 
 # Yes/No checking function
 def yes_no(question_text):
@@ -35,10 +36,58 @@ def instructions():
     print()
 
 
+# Function to generate questions and score
+def generate_question():
+    # list of numbers
+    num_list = [['Tahi', "1", "one"], ['Rua', "2", "two"], ['Toru', "3", "three"],
+                ['Whā', "4", "four"], ['Rima', "5", "five"], ["Ono", "6", "six"],
+                ['Whitu', '7', "seven"], ['Waru', "8", "eight"], ['Iwa', "9", "nine"],
+                ["Tekau", "10", "ten"]]
+
+    # Score tracker
+    score = 0
+
+    # Question tracker
+    question = 1
+
+    # Enter to Begin
+    choice = input("Please press <enter> to begin")
+    while choice != "":
+        choice = input("Please press <enter> to begin")
+        print()
+    while choice == "":
+        if choice == "":
+            choice = num_list
+
+        # Shuffle the list
+        random.shuffle(choice)
+        # Ask question
+        for i in choice:
+            print()
+            print(f"Question {question}")
+            answer = input("Please enter the english word for {}: ".format(i[0])).lower()
+            # If it is right show correct and add one to the score
+            if answer == i[1] or answer == i[2]:
+                print("Correct")
+                print()
+                score += 1
+
+
+            # If incorrect give feedback
+            else:
+                print('Incorrect')
+                print(f"The answer was {i[1]}")
+                print()
+            question += 1
+    # show the score
+    return f"Your score was {score} out of ten"
+
+
 # Main routine
 played_before = yes_no("have you done this quiz before? :")
 if played_before == "No":
     instructions()
+    print(generate_question())
 else:
-    print("program continues")
+    print(generate_question())
 
--- 04_Question_V5.py
+++ 04_Question_V5.py
@@ -7,7 +7,7 @@ import random
 
 def generate_question():
     # list of numbers
-    num_list = [['Tahi', "1", "one"], ['Rua', "2", "two"], ['Tora', "3", "three"],
+    num_list = [['Tahi', "1", "one"], ['Rua', "2", "two"], ['Toru', "3", "three"],
                 ['Whā', "4", "four"], ['Rima', "5", "five"], ["Ono", "6", "six"],
                 ['Whitu', '7', "seven"], ['Waru', "8", "eight"], ['Iwa', "9", "nine"],
                 ["Tekau", "10", "ten"]]
@@ -15,8 +15,14 @@ def generate_question():
     # Score tracker
     score = 0
 
+    # Question tracker
+    question = 1
+
     # Enter to Begin
     choice = input("Please press <enter> to begin")
+    while choice != "":
+        choice = input("Please press <enter> to begin")
+        print()
     while choice == "":
         if choice == "":
             choice = num_list
@@ -25,19 +31,22 @@ def generate_question():
         random.shuffle(choice)
         # Ask question
         for i in choice:
-            answer = input("please enter the english word for {}: ".format(i[0])).lower()
+            print()
+            print(f"Question {question}")
+            answer = input("Please enter the english word for {}: ".format(i[0])).lower()
             # If it is right show correct and add one to the score
             if answer == i[1] or answer == i[2]:
                 print("Correct")
                 print()
                 score += 1
 
+
             # If incorrect give feedback
             else:
                 print('Incorrect')
                 print(f"The answer was {i[1]}")
                 print()
-
+            question += 1
     # show the score
     return f"Your score was {score} out of ten"
 
