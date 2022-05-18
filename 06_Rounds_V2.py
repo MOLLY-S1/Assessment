b8a2306ca4a5754e31a0e6ab0f9579de0b4b9414
MOLLY-S1 <sankeym@middleton.school.nz>
2022-05-18 14:16:04 +1200

    Update 06_Rounds_V2.py

--- 06_Rounds_V2.py
+++ 06_Rounds_V2.py
@@ -1,6 +1,7 @@
 """ Component to loop rounds and play again"""
 
 import random
+score = 0
 
 def generate_question():
     # list of numbers
@@ -17,44 +18,39 @@ def generate_question():
 
     # Rounds
     play_again = ""
-
     while play_again != 'x':
 
         # Enter to Begin
-        choice = input("Please press <enter> to begin")
-        while choice != "":
+        while True:
             choice = input("Please press <enter> to begin")
             print()
-        while choice == "":
-            if choice == "":
-                choice = num_list
+            while choice == "":
+                if choice == "":
+                    choice = num_list
 
-            # Shuffle the list
-            random.shuffle(choice)
-            # Ask question
-            for i in choice:
-                print()
-                print(".",f"Question {question}")
-                print()
-                answer = input("Please enter the english word for {}: ".format(i[0])).lower()
-                # If it is right show correct and add one to the score
-                if answer == i[1] or answer == i[2]:
-                    print("!","Correct")
+                # Shuffle the list
+                random.shuffle(choice)
+                # Ask question
+                for i in choice:
                     print()
-                    score += 1
-
-
-                # If incorrect give feedback
-                else:
-                    print("*",'Incorrect')
-                    print(f"The answer was {i[1]}")
+                    print(".",f"Question {question}")
                     print()
-                question += 1
-        # show the score
-        return ("/",f"Your score was {score} out of 10 questions")
+                    answer = input("Please enter the english word for {}: ".format(i[0])).lower()
+                    # If it is right show correct and add one to the score
+                    if answer == i[1] or answer == i[2]:
+                        print("!","Correct")
+                        print()
+                        score += 1
 
-    # Code to play again
-    play_again =  input("\n Do you want to play again?\n "
+
+                    # If incorrect give feedback
+                    else:
+                        print("*",'Incorrect')
+                        print(f"The answer was {i[1]}")
+                        print()
+                    question += 1
+     # Code to play again
+    play_again = input("\n Do you want to play again?\n "
                         "<enter> to play again or 'X' to exit").lower()
 
 
