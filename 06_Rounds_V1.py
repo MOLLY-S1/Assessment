26b1c528328375c7237e8ca6c6b5af38a3e8b87e
MOLLY-S1 <sankeym@middleton.school.nz>
2022-05-18 13:44:17 +1200

    Rounds V1

---
+++ 06_Rounds_V1.py
@@ -0,0 +1,10 @@
+""" Component to loop rounds and play again"""
+
+play_again = ""
+
+while play_again != 'x':
+    print("program continues as expected")
+
+    play_again =  input("\n Do you want to play again?\n "
+                        "<enter> to play again or 'X' to exit").lower()
+
