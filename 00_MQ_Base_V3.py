b739f5115db35d2e0c3c0d1e33089568533a3733
MOLLY-S1 <sankeym@middleton.school.nz>
2022-05-23 14:27:15 +1200

    base update

--- 00_MQ_Base_V3.py
+++ 00_MQ_Base_V3.py
@@ -26,6 +26,7 @@ def yes_no(question_text):
         # If anything else is entered show error message
         else:
             print("Please enter 'Yes' or 'No' ")
+            print()
 
 
 # Function to display instructions
@@ -33,8 +34,10 @@ def instructions():
     print("*** How To Play ***")
     print()
     print("There will be 10 questions to test you knowledge of Māori numbers\n"
-          "Each question can be answered as a integer or a word for that number\n"
-          "At the end of the 10 questions you will be given your score out of ten\n"
+          "Each question can be answered as a integer or a word for that"
+          " number\n"
+          "At the end of the 10 questions you will be given your score out of"
+          " ten\n"
           "You can then press <enter> to play again or 'x' to quit\n"
           "Have fun and good luck! ")
     print()
@@ -44,10 +47,11 @@ def instructions():
 def generate_question():
 
     # list of numbers
-    num_list = [['Tahi', "1", "one"], ['Rua', "2", "two"], ['Toru', "3", "three"],
-                ['Whā', "4", "four"], ['Rima', "5", "five"], ["Ono", "6", "six"],
-                ['Whitu', '7', "seven"], ['Waru', "8", "eight"], ['Iwa', "9", "nine"],
-                ["Tekau", "10", "ten"]]
+    num_list = [['Tahi', "1", "one"], ['Rua', "2", "two"],
+                ['Toru', "3", "three"], ['Whā', "4", "four"],
+                ['Rima', "5", "five"], ["Ono", "6", "six"],
+                ['Whitu', '7', "seven"], ['Waru', "8", "eight"],
+                ['Iwa', "9", "nine"], ["Tekau", "10", "ten"]]
 
     # Score tracker
     score = 0
@@ -74,7 +78,8 @@ def generate_question():
                 print()
                 print(formatter(".", f"Question {question}"))
                 print()
-                answer = input("Enter the english word for {}: ".format(i[0])).lower()
+                answer = input("Enter the english word for {}: ".
+                               format(i[0])).lower()
                 # If it is right show correct and add one to the score
                 if answer == i[1] or answer == i[2]:
                     print(formatter("!", "Correct"))
@@ -88,7 +93,8 @@ def generate_question():
                     print()
                 question += 1
                 # show the score
-            print(formatter("/", f"Your score was {score} out of 10 questions"))
+            print(formatter
+                  ("/", f"Your score was {score} out of 10 questions"))
             if 4 >= score:
                 print("You should probably do some more studying!")
             elif 4 < score <= 6:
